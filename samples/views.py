d4956c14bc04845e32b3203ea7f8b2813577f71b
noormohammed <noormohammed.noolkar@gmail.com>
2022-10-20 13:56:04 +0100

    Minor updates in view

--- samples/views.py
+++ samples/views.py
@@ -7,9 +7,7 @@ class SamplesView(View):
 
     def index(request):
         answer = do_lots_of_things(
-            999,
-            '123.231',
-            '2023-10-22 11:55:12'
+            999, '123.231', '2023-10-22 11:55:12'
         )
         return HttpResponse(
             f'Result of do_lots_of_things is: <strong>{answer}</strong>'
